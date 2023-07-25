1b5a43cf27de878feb15ca766154af449b0418b3
NeelTheBomb <77640484+NeelTheBomb@users.noreply.github.com>
2023-07-25 09:17:29 -0400

    Create reinforcement.py

---
+++ reinforcement.py
@@ -0,0 +1,32 @@
+class Solution:
+    def create_action_dict(self, num_players, num_actions):
+            #type num: two integer values
+            #return type: int dictionary
+            Dict = {}
+            count = 0
+            for i in range(len(num_players)):
+                for j in range(len(num_actions)):
+                    count = count + 1
+                    tup = ()
+                    for b in range(len(num_players)):
+                        tup = tup + 0
+                    tup[i] = j
+                    print(tup)
+                    Dict[count] = tup
+                    
+                         
+            
+            #TODO: Write code below to return a dictionary with the solution to the prompt.
+            pass
+    
+def main():
+    input1 = input()
+    input1= int(input1)
+    input2= input()
+    input2=int (input2)
+    tc1 = Solution()
+    ans = tc1.create_action_dict(input1, input2)
+    print(ans)
+
+if __name__ == "__main__":
+    main()
